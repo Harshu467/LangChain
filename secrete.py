42070f9af8a8b2abec2b5f5592ac482a067cb466
Harsh Upadhye <harshupadhye2832@gmail.com>
2023-08-23 11:51:21 +0530

    updated

--- secrete.py
+++ secrete.py
@@ -1,7 +1,1 @@
-OPEN_API_KEY = "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"
-
-# from langchain.llms import OpenAI
-
-# llm = OpenAI(openai_api_key="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")
-
-# print(llm.predict("Who won the world cup in 2011?"))
+OPEN_API_KEY = "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"
